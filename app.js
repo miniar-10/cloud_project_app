bb5906785a171b762740e9c9b0a5b442c18fd1fe
miniar-10 <lasmar.miniar9@gmail.com>
2023-01-02 00:29:58 +0100

    first commit

--- app.js
+++ app.js
@@ -8,14 +8,30 @@ var myConnection = require('express-myconnection')
 
 var config = require('./config')
 var dbOptions = {
- host: config.database.host,
- user: config.database.user,
- password: config.database.password,
- port: config.database.port,
- database: config.database.db,
+ host: "my-db-instance.c0rznvbeuzkj.us-east-1.rds.amazonaws.com",
+ user: "admin",
+ password: "adminadmin",
+//  port: config.database.port,
+ database: "nodejs_crud",
 }
 
 app.use(myConnection(mysql, dbOptions, 'pool'))
+
+// console.log('MySQL Database is connected Successfully');
+//         connection.query("CREATE DATABASE IF NOT EXISTS testing", function (err) {
+//             if (err) throw err;
+//             console.log("Database created");
+
+//             connection.query("USE testing", function (err) {
+//                 if (err) throw err;
+//                 console.log("Database testing is being used");
+//                 var sql = "CREATE TABLE IF NOT EXISTS sample_data (id INT(10) NOT NULL, first_name VARCHAR(250) NOT NULL, last_name VARCHAR(250) NOT NULL, age VARCHAR(30) NOT NULL, gender VARCHAR(30) NOT NULL)";
+//                 connection.query(sql, function (err, result) {
+//                     if (err) throw err;
+//                     console.log("Table created");
+//                 });
+//             });
+//         });
 
 app.set('view engine', 'ejs')
 
